a67aa99535194368ebed2ec16d168ad7e68d3719
Dror <drorp24@gmail.com>
2018-05-10 15:29:46 +0300

    sendGrid webhook: log responses

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser").default;
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -9,6 +12,23 @@ const Survey = mongoose.model("surveys");
 module.exports = app => {
 	app.get("/api/surveys/thanks", (req, res) => {
 		res.send("Thanks for your feedback!");
+	});
+
+	app.post("/api/surveys/webhooks", (req, res) => {
+		const events = _.map(req.body, ({ email, url }) => {
+			const pathname = new URL(url).pathname;
+			const p = new Path("/api/surveys/:surveyId/:choice");
+			const match = p.test(pathname);
+			if (match) {
+				return {
+					email,
+					surveyId: match.surveyId,
+					choice: match.choice
+				};
+			}
+		});
+
+		console.log(events);
 	});
 
 	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
